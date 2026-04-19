a21d9f54815ea53ba440cddb014f4b9d9ac78cd1
SANTHOSH A <arussanthosh@gmail.com>
2026-04-19 22:19:15 +0530

    Add Button 2

--- LEARING/19-04-2026/FIRST GUI/App.java
+++ LEARING/19-04-2026/FIRST GUI/App.java
@@ -5,13 +5,15 @@ class App extends Frame{
         Button btn = new Button("Hello");
         Button btn1 = new Button("SanStudio");
         add(btn);
-        setLayout (new FlowLayout());
-        setVisible(true);
-        setSize(400, 400);
         add(btn1);
+
+
+
         setLayout (new FlowLayout());
         setVisible(true);
         setSize(400, 400);
+        
+      
     }
 
     public static void main(String[] args) {
