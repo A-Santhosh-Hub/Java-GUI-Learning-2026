b034083d240ab38a2627d09d06761f6a38f2a7dd
SANTHOSH A <arussanthosh@gmail.com>
2026-04-19 22:23:53 +0530

    add Button 3

--- LEARING/19-04-2026/FIRST GUI/App.java
+++ LEARING/19-04-2026/FIRST GUI/App.java
@@ -4,8 +4,10 @@ class App extends Frame{
     App(){
         Button btn = new Button("Hello");
         Button btn1 = new Button("SanStudio");
+        Button btn2 = new Button("Open to See the Work");
         add(btn);
         add(btn1);
+        add(btn2);
 
 
 
