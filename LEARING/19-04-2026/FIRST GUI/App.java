c8e37a2e67edaa92034a08e0fc21dc646e63dc9d
SANTHOSH A <arussanthosh@gmail.com>
2026-04-19 22:41:02 +0530

    19-04 -2026
    Today Learn GUI (Graphical User Interface) components like buttons, frames.

--- LEARING/19-04-2026/FIRST GUI/App.java
+++ LEARING/19-04-2026/FIRST GUI/App.java
@@ -1,14 +1,28 @@
 import java.awt.*;
+import java.awt.event.*;
+import java.net.URI;
+
 
 class App extends Frame{
     App(){
         Button btn = new Button("Hello");
         Button btn1 = new Button("SanStudio");
         Button btn2 = new Button("Open to See the Work");
+
         add(btn);
         add(btn1);
         add(btn2);
 
+
+        btn2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                try{
+                    Desktop.getDesktop().browse(new URI("https://sanstudio-hub.github.io/SanStudio-Hub/in"));
+                } catch(Exception ex){
+                System.out.println("Error"+ex);
+                }
+            }
+        });
 
 
         setLayout (new FlowLayout());
@@ -22,3 +36,6 @@ class App extends Frame{
        new App();
     }
 }
+
+
+
