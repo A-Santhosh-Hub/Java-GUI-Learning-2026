6c627565f92f4625bb65bb346bd0e851ac419c62
SANTHOSH A <arussanthosh@gmail.com>
2026-04-20 23:42:46 +0530

    Recall

--- LEARING/20-04-2026/App.java
+++ LEARING/20-04-2026/App.java
@@ -6,25 +6,26 @@ import java.awt.*;
 import java.awt.event.*;
 import java.net.URI.*;
 
-
 class App extends Frame{
 
-App(){
+    App(){
+        Button btn=new Button("HAI");
+        Button btn1 = new Button("Well Come");
+        Button btn2 = new Button("SanStudio");
+
+    add(btn);
+    add(btn1);
+    add(btn2);
+
+    setLayout(new FlowLayout());
+    setVisible(true);
+    setSize(400,600);
+
+    }
 
 
-Button btn = new Button("Hai");
-Button btn1 =new Button("Well Come");
-Button btn2 =new Button("SanStudio");
 
-add(btn);
-add(btn1);
-add(btn2);
-setLayout(new FlowLayout());
-setVisible(true);
-setSize(400,600);
-
-}
     public static void main(String[] args) {
-    new App();    
+        new App();
     }
 }
