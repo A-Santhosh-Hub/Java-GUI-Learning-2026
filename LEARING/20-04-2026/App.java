e560dcb0220703e979c63c2600b9b2120b0d4db2
SANTHOSH A <arussanthosh@gmail.com>
2026-04-20 23:37:36 +0530

    Day 02

---
+++ LEARING/20-04-2026/App.java
@@ -0,0 +1,30 @@
+
+//Recall of Day 19-04-2026
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URI.*;
+
+
+class App extends Frame{
+
+App(){
+
+
+Button btn = new Button("Hai");
+Button btn1 =new Button("Well Come");
+Button btn2 =new Button("SanStudio");
+
+add(btn);
+add(btn1);
+add(btn2);
+setLayout(new FlowLayout());
+setVisible(true);
+setSize(400,600);
+
+}
+    public static void main(String[] args) {
+    new App();    
+    }
+}
