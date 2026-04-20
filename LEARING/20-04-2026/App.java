768b73960bf6ac417bd50a13e4c33a9186d1e1d3
SANTHOSH A <arussanthosh@gmail.com>
2026-04-20 23:54:33 +0530

    Recall

--- LEARING/20-04-2026/App.java
+++ LEARING/20-04-2026/App.java
@@ -1,29 +1,38 @@
 
 //Recall of Day 19-04-2026
 
-
 import java.awt.*;
 import java.awt.event.*;
-import java.net.URI.*;
+import java.net.URI;
 
-class App extends Frame{
+class App extends Frame {
 
-    App(){
-        Button btn=new Button("HAI");
-        Button btn1 = new Button("Well Come");
+    App() {
+        Button btn = new Button("HAI");
+        Button btn1 = new Button("Welcome");
         Button btn2 = new Button("SanStudio");
 
-    add(btn);
-    add(btn1);
-    add(btn2);
+        // Action for btn
+        btn2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Desktop.getDesktop().browse(
+                        new URI("https://sanstudio-hub.github.io/SanStudio-Hub/in")
+                    );
+                } catch (Exception ex) {
+                    System.out.println("Error: " + ex);
+                }
+            }
+        });
 
-    setLayout(new FlowLayout());
-    setVisible(true);
-    setSize(400,600);
+        add(btn);
+        add(btn1);
+        add(btn2);
 
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(400, 600);
     }
-
-
 
     public static void main(String[] args) {
         new App();
